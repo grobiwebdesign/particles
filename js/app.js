536240b8ec5b88874a7e6e54905006e2978737d9
Graeme Robinson <1robig68@solent.ac.uk>
2018-12-20 18:38:17 +0000

    Landing Page with Particles.JS
    particles won't be visible on GitHub, but will once I update my website on a separate server

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js is functioning');
+});
